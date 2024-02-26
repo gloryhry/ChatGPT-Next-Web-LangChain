2aed290c3242e03cf9819a22cca5ddf65070d670
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-02-26 23:43:57 +0800

    Update openai.ts

--- app/client/platforms/openai.ts
+++ app/client/platforms/openai.ts
@@ -192,7 +192,7 @@ export class ChatGPTApi implements LLMApi {
           message.content.push({
             type: "image_url",
             image_url: {
-              url: `${url}${v.image_url}`,
+              url: encodeURI(`${url}${v.image_url}`),
             },
           });
         }
